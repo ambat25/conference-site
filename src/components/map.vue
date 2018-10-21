12e2d5d546705eccb7c320c441bb33cf84773ad8
ambat25 <25ambat25@gmail.com>
2018-10-21 11:56:33 +0100

    updated map to mambayya house

--- src/components/map.vue
+++ src/components/map.vue
@@ -1,9 +1,11 @@
 <template>
     <section class="map" style="margin-top: 100px;">
-        <iframe title="Event Location in Map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3940.294761576666!2d7.494947614293468!3d9.03685349351543!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x104e0b8756a909c3%3A0x3eda62fe55f6387b!2sAyalla+Hotels!5e0!3m2!1sen!2sng!4v1539777840614"
-        width="100%" height="450"
-        frameborder="0"
-        style="border:0" allowfullscreen>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1951.2073185092363!2d8.504931405264255!3d12.014950070981223!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x11ae80bf6106545b%3A0x1588f6521c728626!2sMambayya+House!5e0!3m2!1sen!2sng!4v1540119257738" 
+        width="100%" 
+        height="450"
+        frameborder="0" 
+        style="border:0" 
+        allowfullscreen>
         </iframe>
     </section>
 </template>
