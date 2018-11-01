03adb7bdf5c8a0f6488879fae0832757ce429f2e
ambat25 <25ambat25@gmail.com>
2018-11-01 22:05:51 +0100

    added partners list

--- src/components/sponsors.vue
+++ src/components/sponsors.vue
@@ -1,10 +1,18 @@
 <template>
     <section class="content sponsors has-text-centered">
         <h1 class="title has-text-centered">SPONSORS</h1>
-        <h3 class="title" style="margin-top: 50px">Event Sponsors</h3>
-        <img v-for="(sponsor,index) in sponsors" :key="index" :src="sponsor.logo" alt="" style="width: 140px; ">
-        <h3 class="title" style="margin-top: 50px">Event Partners</h3>
-        <img v-for="(partner,index) in partners" :key='index' :src="partner.logo" alt="" style="width: 100px; max-height: 100px">
+        <div>
+            <h3 class="title" style="margin-top: 50px">Event Sponsors</h3>
+            <img v-for="(sponsor,index) in sponsors" :key="index" :src="sponsor.logo" alt="" style="width: 140px; ">
+        </div>
+            <h3 class="title" style="margin-top: 50px">Event Partners</h3>
+        <div class="columns is-multiline is-gapless">
+            <div class="column is-one-fifth" v-for="(partner,index) in partners" :key='index'>
+                <!-- <div class="columns is-mobile"> -->
+                    <img :src="partner.logo" class=" is-half" alt="" style="width: 100px; max-height: 100px">
+                <!-- </div> -->
+            </div>
+        </div>
         <br>
         <br>
         <br>
@@ -17,28 +25,37 @@ export default {
         return {
             partners : [
                 {
-                    "name": "Startup Kano",
-                    "logo": require('../assets/partners/partner1.png')
+                    "logo": require('../assets/partners/partner1.jpg')
                 },
                 {
-                    "name": "Startup Kano",
                     "logo": require('../assets/partners/partner2.png')
                 },
                 {
-                    "name": "Startup Kano",
                     "logo": require('../assets/partners/partner3.png')
                 },
                 {
-                    "name": "Startup Kano",
                     "logo": require('../assets/partners/partner4.png')
                 },
                 {
-                    "name": "Startup Kano",
                     "logo": require('../assets/partners/partner5.png')
                 },
                 {
-                    "name": "Startup Kano",
-                    "logo": require('../assets/partners/partner1.png')
+                    "logo": require('../assets/partners/partner8.jpg')
+                },
+                {
+                    "logo": require('../assets/partners/partner7.png')
+                },
+                {
+                    "logo": require('../assets/partners/partner9.png')
+                },
+                {
+                    "logo": require('../assets/partners/partner10.jpg')
+                },
+                {
+                    "logo": require('../assets/partners/partner11.jpg')
+                },
+                {
+                    "logo": require('../assets/partners/partner12.jpg')
                 }
             ],
             sponsors : [
@@ -51,3 +68,9 @@ export default {
     }
 }
 </script>
+<style scoped>
+.is-vertical-center {
+  display: flex;
+  align-items: center;
+}
+</style>
