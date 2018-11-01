02ac81cf8d01e351a363bf684756db5b5e753a61
ambat25 <25ambat25@gmail.com>
2018-11-01 22:39:57 +0100

    updated the event date on the main page

--- src/components/body_hero.vue
+++ src/components/body_hero.vue
@@ -3,7 +3,7 @@
         <div class="container has-text-centered">
             <img class="title" id="img" alt="GDG DevFest North West 2018 logo" src="../assets/logo.svg">
             <div >
-                <p class="title">Kano, Nigeria<br> November 17, 2018 </p>
+                <p class="title">Kano, Nigeria<br> November 18, 2018 </p>
 
             </div>
         </div>
