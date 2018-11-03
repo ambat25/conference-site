a6f8ca13797f94053e84e3104b3219b1b2af4b5c
ambat25 <25ambat25@gmail.com>
2018-11-03 17:49:17 +0100

    updated the organizers list

--- src/components/organizers.vue
+++ src/components/organizers.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="content">
         <h1 class="title has-text-centered">ORGANIZERS</h1>
-        <div class="columns">
+        <div class="columns is-multiline">
             <div class="column is-4">
                 <div class="columns is-mobile">
                     <div class="column is-6">
@@ -31,7 +31,7 @@
                     </div>
                     <div class="column is-6">
                         <figure class="image is-128x128">
-                            <img class="is-rounded" src="../assets/organizers/calistus.jpg" alt="Umar's picture">
+                            <img class="is-rounded" src="../assets/organizers/calistus.jpeg" alt="Umar's picture">
                             <h3>Calistus</h3>
                             <p class="organizers_desc">Sokoto, Nigeria</p>
                         </figure>
@@ -56,15 +56,69 @@
                     </div>
                 </div>
             </div>
+            <div class="column is-4">
+                <div class="columns is-mobile">
+                    <div class="column is-6">
+                        <figure class="image is-128x128">
+                            <img class="is-rounded" src="../assets/organizers/ruqayya.jpg" alt="Bulama's picture">
+                            <h3>Ruqayya dan Maigoro</h3>
+                            <p class="organizers_desc">Sokoto, Nigeria</p>
+                        </figure>
+                    </div>
+                    <div class="column is-6">
+                        <figure class="image is-128x128">
+                            <img class="is-rounded" src="../assets/organizers/maryam.jpg" alt="Auwal picture">
+                            <h3>Maryam Usman Tahir</h3>
+                            <p class="organizers_desc">Katsina, Nigeria</p>
+                        </figure>
+                    </div>
+                </div>
+            </div>
+            <div class="column is-4">
+                <div class="columns is-mobile">
+                    <div class="column is-6">
+                        <figure class="image is-128x128">
+                            <img class="is-rounded" src="../assets/organizers/hashim.jpg" alt="Farida's picture">
+                            <h3>Muhammad Hashim</h3>
+                            <p class="organizers_desc">Kano, Nigeria</p>
+                        </figure>
+                    </div>
+                    <div class="column is-6">
+                        <figure class="image is-128x128">
+                            <img class="is-rounded" src="../assets/organizers/aliyu.jpg" alt="Umar's picture">
+                            <h3>Aliyu Muhammad</h3>
+                            <p class="organizers_desc">Kano, Nigeria</p>
+                        </figure>
+                    </div>
+                </div>
+            </div>
+            <div class="column is-4">
+                <div class="columns is-mobile">
+                    <div class="column is-6">
+                        <figure class="image is-128x128">
+                            <img class="is-rounded" src="../assets/organizers/musty.jpg" alt="Usman's picture">
+                            <h3>Mustapha Muhammad Garba</h3>
+                            <p class="organizers_desc">Kano, Nigeria</p>
+                        </figure>
+                    </div>
+                    <div class="column is-6">
+                        <figure class="image is-128x128">
+                            <img class="is-rounded" src="../assets/organizers/abdulhakeem.jpeg" alt="Pius's picture">
+                            <h3>Abdulhakeem Bashir</h3>
+                            <p class="organizers_desc">Katsina, Nigeria</p>
+                        </figure>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div class="level event_info_wrapper">
+        <!-- <div class="level event_info_wrapper">
             <div class="level-left event_link">
                 <a target="_blank" href="#" class="level-item " rel="noopener">
                     <span class="is-vertical event_info_link" >VIEW FULL TEAM</span>
                     <b-icon icon="arrow-right-bold-circle" class="event_link_icon"></b-icon>
                 </a>
             </div>
-        </div>
+        </div> -->
     </div>
 </template>
 
