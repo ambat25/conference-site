b3b48386ddc3e70c1ab239aada9cd1715d57c988
ambat25 <25ambat25@gmail.com>
2018-11-04 14:08:00 +0100

    updated what is GDG

--- src/components/chapters.vue
+++ src/components/chapters.vue
@@ -11,9 +11,9 @@
                 <div class="column is-half-tablet">
                     <h1 class="title">WHAT IS GDG?</h1>
                     <p class="body">
-                        Google Developers Group (GDG) - is open and volunteer geek community
-                        who create exciting projects and share experience about Google technologies
-                        with a passion
+                        GDGs are local groups of developers who are specifically interested in Google products and APIs. Each local group is called a GDG chapter and can host a variety of technical activities for developers - from just a few people getting together to watch our latest videos, to large gatherings with demos and tech talks, to hackathons.
+                        <br>
+                        DevFests are community-led, developer events hosted by GDG chapters around the globe focused on community building and learning about Google’s technologies. Each DevFest is inspired by and uniquely tailored to the needs of the developer community and region that hosts it.
                     </p>
                 </div>
             </div>
